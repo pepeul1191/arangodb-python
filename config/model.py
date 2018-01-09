77e0b24fb5282ba16d6ebfaf328ee851938717ef
Pepe Valdivia <jvaldivia@softweb.pe>
2018-01-09 09:34:56 -0500

    Acceso a DB sin DAOs

--- config/model.py
+++
@@ -1,5 +0,0 @@
-from config.database import db
-
-class Model:
-	def __init__(self):
-		self.db = db
--- views/empresa.py
+++ views/empresa.py
@@ -2,11 +2,22 @@
 # -*- coding: utf-8 -*-
 import json
 from bottle import Bottle, request
-from models.empresa import Empresa
+from config.database import db
 
 empresa_view = Bottle()
 
 @empresa_view.route('/listar', method='GET')
 def listar():
-  empresas = Empresa().listar()
-  return json.dumps(empresas)
+  rpta = []
+  empresas = db.collection('empresas')
+  for empresa in empresas:
+    rpta.append({'_id': empresa['_key'], 'razon_social': empresa['razon_social']})
+  return json.dumps(rpta)
+
+@empresa_view.route('/guardar', method='GET')
+def guardar():
+  txn = db.transaction(write='empresas')
+  txn.collection('empresas').insert({'razon_social': 'Jake'})
+  txn.collection('empresas').insert({'razon_social': 'Jill'})
+  txn.commit()
+  return json.dumps('rpta')
